7e0376c4bb0e9746858fa94de324f23732bc4339
Reyes815 <reyes.rowen30@gmail.com>
2024-05-27 18:16:33 +0800

    result page

--- frontend/src/views/ui/Result.js
+++ frontend/src/views/ui/Result.js
@@ -1,24 +1,24 @@
 import DOMPurify from 'dompurify';
 import { React, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 function Result() {
   const [html, setHtml] = useState('');
   const location = useLocation();
   const { text } = location.state || {};
+  const API_Key = 'AIzaSyB2M82ENZgfYOHWsuBS9NqG3jHyz7xo9TQ';  // Replace with your actual API key
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Replace with your private API key
-        // const genAI = new GoogleGenerativeAI('AIzaSyBDRBBgf0jcTb9MZMWTJcUSqOoP9ZfzxMI');
-        // const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
-        // const prompt = 'Create a wireframe of a home page for an ecommerce website using html with scss';
-        // const result = await model.generateContent(prompt);
-        // const response = await result.response;
-        // const text = await response.text();
-        // Sanitize HTML for security
-        const sanitizedHtml = DOMPurify.sanitize(text);
+        const genAI = new GoogleGenerativeAI(API_Key);
+        const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+        const prompt = text;
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const prompttext = await response.text();
+        const sanitizedHtml = DOMPurify.sanitize(prompttext);
         console.log(sanitizedHtml)
         setHtml(sanitizedHtml);
       } catch (error) {
