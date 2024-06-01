f257a640b2164e66839061b631d945341a1437a6
Reyes815 <reyes.rowen30@gmail.com>
2024-06-01 19:30:47 +0800

    Added a layout for a page with a nav bar but no sidebar

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,19 +1,17 @@
 // ThemeRoutes.js
 import { lazy } from "react";
 import { Navigate } from "react-router-dom";
-import Temporary from "../layouts/temporary.js";
 import { element, exact } from "prop-types";
 
 /****Layouts*****/
 const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
 const OutsideLayout = lazy(() => import("../layouts/OutsideLayout.js"));
+const NoSidebarLayout = lazy(() => import("../layouts/NoSidebarLayout.js"));
 
 /***** Pages ****/
 
 const Registration = lazy(() => import("../views/ui/Registration.js"));
 const User = lazy(() => import("../views/ui/User.js"));
-const Profile = lazy(() => import("../views/ui/Profile.js"));
-const temp = lazy(() => import("../layouts/temporary.js"));
 const Login = lazy(() => import("../views/ui/Login.js"));
 const Result = lazy(() => import("../views/ui/Result.js"));
 
@@ -24,21 +22,22 @@ const ThemeRoutes = [
     element: <FullLayout />,
     children: [
       { path: "/user", exact: true, element: <User/> }, 
-      { path: "/profile", exact: true, element: <Profile/> },
+    ],
+  },
+  {
+    element: <NoSidebarLayout />,
+    children: [
+      { path: "/result", exact: true, element: <Result/> },
     ],
   },
   {
     path: "/",
     element: <OutsideLayout />,
     children: [
-      // { path: "/", element: <Navigate to="/temp" /> },
-      // { path: "/temp", exact: true, element: <Temporary /> },
       { path: "/", element: <Navigate to="/user" /> },
-      { path: "/user", exact: true, element: <User /> },
-      { path: "/", element: <Navigate to="/login" /> },
       { path: "/login", exact: true, element: <Login /> },
       { path: "/register", exact: true, element: <Registration /> },
-      { path: "/result", exact: true, element: <Result/> },
+      
     ],
   },
 ];
