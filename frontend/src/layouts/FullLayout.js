6afb55f1b6d0276a1bfdc26a8c7b8f2c883edc6a
Reyes815 <reyes.rowen30@gmail.com>
2024-09-02 20:36:32 +0800

    added sample editor

--- frontend/src/layouts/FullLayout.js
+++ frontend/src/layouts/FullLayout.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import { Container } from "reactstrap";
 import Sidebar from "./Sidebar";
+import BpmnDiagram from "./Diagram_Editor";
 
 const FullLayout = () => {
   return (
@@ -12,10 +13,12 @@ const FullLayout = () => {
           {/********header**********/}
           <Header />
           {/********Header End**********/}
+          <BpmnDiagram/>
           {/********Sidebar**********/}
-          <aside className="sidebarArea shadow" id="sidebarArea">
+          {/* <aside className="sidebarArea shadow" id="sidebarArea">
             <Sidebar />
-          </aside>
+          </aside> */}
+
           {/********Sidebar End**********/}
           {/********Middle Content**********/}
           <Container className="p-4 wrapper" fluid>
