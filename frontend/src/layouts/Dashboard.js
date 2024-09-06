c64284e264d610294ee7fc1d502d81db0c153972
Venn Dominic A. Dela Peña <134032009+VennDominicAdelaPena@users.noreply.github.com>
2024-09-07 04:13:37 +0800

    Dashboard

--- frontend/src/layouts/Dashboard.js
+++ frontend/src/layouts/Dashboard.js
@@ -1,10 +1,11 @@
 import { Grid } from '@mui/material';
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import Themes from '../components/popup/Themes.js';
 import BusinessRules from '../components/popup/BusinessRules.js';
 import Error from '../components/popup/Error.js';
-
+import axios from 'axios';
+import { UserContext } from "../Usercontext";
 const Dashboard = () => {
   const navigate = useNavigate();
   const [popupThemesOpen, setThemesPopupOpen] = useState(false);
@@ -12,7 +13,8 @@ const Dashboard = () => {
   const [selectedOption, setSelectedOption] = useState('');
   const [submittedText, setSubmittedText] = useState('');
   const [showError, setShowError] = useState(false);
-
+  const [diagrams, setDiagrams] = useState([]);
+  const { user_id } = useContext(UserContext); 
   const getColorsByTheme = (selectedOption) => {
     switch (selectedOption) {
         case "Pastel":
@@ -59,19 +61,40 @@ const Dashboard = () => {
     setShowError(false);
   };
 
+  useEffect(() => {
+    if (user_id) {axios.get(`/diagrams/${user_id}`)
+        .then(response => {
+            setDiagrams(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching diagrams:', error);
+        });
+      }
+}, [user_id]);
+
+const listStyle = {
+  listStyleType: 'none', 
+  padding: 0,            
+  margin: 0              
+};
   return (
     <div className="p-3">
       <h2>Dashboard</h2>
       <div className="d-flex flex-column align-items-start"> {/* Changed to flex-column for stacking */}
-        <button className="button" onClick={handleThemesOnClick}>
+        {/* <button className="button" onClick={handleThemesOnClick}>
           Import
         </button>
         {popupThemesOpen && <Themes onClose={handleThemesClosePopup} onSubmit={handleThemesPopupSubmit} />}
         <button className="button" onClick={handleBisRulesOnClick}>
           Save
-        </button>
-        {popupBisRulesOpen && <BusinessRules onClose={handleBisRulesClosePopup} onSubmit={handleBisRulesSubmit} />}
-        {showError && <Error onClose={handleErrorClose} />}
+        </button> */}
+        {/* {popupBisRulesOpen && <BusinessRules onClose={handleBisRulesClosePopup} onSubmit={handleBisRulesSubmit} />}
+        {showError && <Error onClose={handleErrorClose} />} */}
+      <ul style={listStyle}>
+        {diagrams.map((diagram, index) => (
+          <li key={index}>{diagram.name}</li>
+        ))}
+      </ul>
       </div>
     </div>
   );
